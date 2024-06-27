9391442f5bb112a923e5229403630cc482ff0387
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-06-27 16:00:54 -0500

    push task 5

--- Node_JS/5-http.js
+++ Node_JS/5-http.js
@@ -1,10 +1,25 @@
 const http = require('http');
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+const DATABASE = args[0];
 const hostname = '127.0.0.1';
 const port = 1245;
-const app = http.createServer((req, res) => {
+
+const app = http.createServer(async (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');    
+    const { url } = req;
+    if (url === '/') {
+      res.write('Hello Holberton School!');
+      try {
+        const students = await countStudents(DATABASE);
+        res.end(`${students.join('\n')}`);
+      } catch (error) {
+        res.end(error.message);
+      }
+    }
+    res.statusCode = 404;
+    res.end();
 });
 app.listen(port, hostname, () => { });
 module.exports = app;
